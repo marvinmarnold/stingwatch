3218755ededfc1408be1ba3dc85cf315f6a3e9f0
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-20 21:36:23 -0600

    GPS

--- cordova/gsm.js
+++ cordova/gsm.js
@@ -5,56 +5,54 @@ if(Meteor.isCordova) {
     startRefreshing();
   });
 
-  var refresh = function() {
-    if(termsAccepted()) {
-      telephony.getCid(function(result) {
-        var gsmReading;
-        // Meteor.call('_log', result.lac || "cat")
-        if(result.phoneType === Catcher.READING_TYPES.GSM) {
-          gsmReading = {
-            commonReading: {
-              deviceId: DeviceId.get(),
-              readingType: Catcher.READING_TYPES.GSM,
-              deviceScannerId: 2,
-            },
-            mcc: parseInt(result.mcc) || -1,
-            mnc: parseInt(result.mnc) || -1,
-            lac: parseInt(result.lac) || -1,
-            cid: parseInt(result.cid) || -1,
-            psc: parseInt(result.psc) || -1,
-          }
-        } else {
-          gsmReading = {
-            commonReading: {
-              deviceId: DeviceId.get(),
-              readingType: Catcher.READING_TYPES.GSM,
-              deviceScannerId: 3,
-              debug: result.phoneType
-            },
-            mcc: parseInt(result.phoneType),
-            mnc: parseInt(result.phoneType),
-            cid: parseInt(result.phoneType),
-            lac: parseInt(result.phoneType),
-          }
-        }
 
-        // Meteor.call('_debug', telephonyEntry)
+}
 
-        Meteor.call('catcher/readings/insert', gsmReading)
-      }, function(error) {
-        _log("gsm error")
-      //   _debug(error)
-      })
-    }
+var startRefreshing = function() {
+  refresh();
+
+  Meteor.setTimeout(function () {
+    // Meteor.call('_log', 'log working')
+
+    startRefreshing()
+  }, refreshPeriod);
+}
+
+var refresh = function() {
+  if(termsAccepted()) {
+    telephony.refresh(result => {
+
+      if(result.phoneType === Catcher.READING_TYPES.GSM) {
+        navigator.geolocation.getCurrentPosition(pos => {
+          insertGSMReading(result, pos)
+        },
+        error => {
+          insertGSMReading(result, {coords: {latitude: -99999, longitude: -99999}})
+        }, {maximumAge:600000, timeout:30000});
+      }
+
+    }, error => {
+      // _log("gsm error")
+    //   _debug(error)
+    });
+  }
+}
+
+var insertGSMReading = function(result, pos) {
+  var gsmReading = {
+    commonReading: {
+      deviceId: DeviceId.get(),
+      readingType: Catcher.READING_TYPES.GSM,
+      deviceScannerId: 2,
+    },
+    mcc: parseInt(result.mcc),
+    mnc: parseInt(result.mnc),
+    lac: parseInt(result.lac),
+    cid: parseInt(result.cid),
+    psc: parseInt(result.psc),
+    latitude: pos.coords.latitude,
+    longitude: pos.coords.longitude
   }
 
-  var startRefreshing = function() {
-    refresh();
-
-    Meteor.setTimeout(function () {
-      // Meteor.call('_log', 'log working')
-
-      startRefreshing()
-    }, refreshPeriod);
-  }
+  Meteor.call('catcher/readings/insert', gsmReading)
 }
