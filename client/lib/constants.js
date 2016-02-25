6e49f89a7808ac740c2877798ec08ab98fb29d57
Marvin Arnold <marvinmarnold@gmail.com>
2016-02-24 20:02:03 -0600

    Watching page w/o data

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -1,6 +1,12 @@
-TERMS_ACCEPTED = 'terms-accepted'
 INTRO_COMPLETED = 'intro-completed'
 INTRO_SLIDE_NUM = 'intro-slide-number' // slide currently being viewed
 NUM_INTRO_SLIDES = 6 // total number of slides
-GEEK_MODE_ENABLED = 'geek-mode-enabled'
+
+TERMS_ACCEPTED = 'terms-accepted'
 TERMS_REJECTED = 'terms-rejected'
+
+GEEK_MODE_ENABLED = 'geek-mode-enabled'
+
+STATUS = 'status'
+STATUS_WATCHING = 'status-watching'
+STATUS_DANGER = 'status-danger'
--- client/pages/status-page.jsx
+++ client/pages/status-page.jsx
@@ -1,6 +1,26 @@
 StatusPage = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+      Session.setDefault(STATUS, STATUS_WATCHING)
+
+    // if(Catcher.inDanger()) {
+    //   Session.set(DETECTION_STATUS, DETECTION_STATUS_DANGER)
+    // } else {
+    //   Session.set(DETECTION_STATUS, DETECTION_STATUS_WATCHING)
+    // }
+
+    return {
+      detectionStatus: Session.get(STATUS)
+    };
+  },
 
   render() {
-    return <div><h1>Status Page</h1></div>
+    if(this.data.detectionStatus === STATUS_WATCHING) {
+      return <StatusWatching />
+    } else if(this.data.detectionStatus === STATUS_DANGER) {
+      return <StatusDanger />
+    }
   }
-})
+
+});
