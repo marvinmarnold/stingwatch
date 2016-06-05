ab308e35b4e17a71bc76e6f9320aa720e63778f3
Marvin Arnold <marvinmarnold@gmail.com>
2016-06-05 18:29:25 -0500

    Do not attempt to insert malformed readings

--- imports/startup/client/device-id.js
+++ imports/startup/client/device-id.js
@@ -2,11 +2,10 @@ import { DeviceId } from 'meteor/marvin:device-id';
 
 export function startupDeviceId() {
   // Generate a deviceId client side
+  console.log('startupDeviceId');
   DeviceId.gen((error, deviceId) => {
+    console.log('generated id: ' + deviceId);
     // console.log('deviceId error');
     // console.log(error);
-    //
-    // console.log('deviceId');
-    // console.log(deviceId);
   });
 }
